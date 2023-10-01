388c46b4ea5239c87a7b8c919f49c94e25d0f4a2
Jayasurya-SM <jayasurya365@outlook.com>
2023-10-01 23:09:31 +0530

    css and jsx updated

--- dropdown/src/App.jsx
+++ dropdown/src/App.jsx
@@ -5,15 +5,17 @@ function App() {
   const [color, setColor] = useState("black");
 
   return (
-    <>
+    <div>
+      <h1>Color changer</h1>
       <select value={color} onChange={(e) => setColor(e.target.value)}>
+        <option></option>
         <option>Red</option>
         <option>Green</option>
         <option>Blue</option>
       </select>
 
       <p style={{ color }}>This is a colored paragraph.</p>
-    </>
+    </div>
   );
 }
 
